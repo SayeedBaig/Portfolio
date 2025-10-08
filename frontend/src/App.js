d577fae1146b812016f6f63719ef5ba0e22b29c6
SayeedBaig <baigsayeed06@gmail.com>
2025-10-08 18:06:54 +0530

    fix:removed unused variables from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import Hero from './component/Hero';
 import About from './component/About';
 import Skills from './component/Skills';
 import ProjectList from './component/projectList';
-import Experience from './component/Education';
 import Contact from './component/Contact';
 import Education from './component/Education';
 
